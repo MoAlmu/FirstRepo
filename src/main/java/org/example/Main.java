caae52141e907250d8a21ea443b4c551f5f9f21e
mohammad.almustafa <mohammad.almustafa@adesso.de>
2024-03-11 23:58:53 +0100

    commit from dev 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hallo from the another branch");
         System.out.println("Hallo from amend branch ");
+        System.out.println("Hallo from amend branch developer 1 ");
     }
 }
